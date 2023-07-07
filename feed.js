f2207a33b125a18328ecc00baab628038f0fbc9d
Emmanuel Adeniyi <angeloluemmanuel@gmail.com>
2023-07-07 02:10:23 +0100

    Add New Post Function

--- feed.js
+++ feed.js
@@ -6,6 +6,19 @@ const navLinks = document.querySelectorAll(".link");
 // New Post types
 const postList = document.querySelectorAll(".post-link");
 
+// New Post Button
+const newPost = document.querySelector("#newPost");
+
+// Modal New Post
+const postBox = document.querySelector(".post-box");
+
+const postArea = document.querySelector("#postArea");
+
+// Close Icon
+const closeIcon = document.querySelector(".close-btn");
+
+// xxxxxxxxx Variables End xxxxxxxxxx
+
 
 // Remove active class
 const removeActiveClass = () => {
@@ -42,3 +55,26 @@ for (var i = 0; i < postList.length; i++) {
   postList[i].classList.add(colorList[count]);
   count++;
 }
+
+// OPEN NEW POST MODAL
+newPost.addEventListener("click", () => {
+  postBox.style.display = "grid";
+  if (postBox.style.display == "grid") {
+    // disable background scroll on modal open
+    document.body.style.overflowY = "hidden";
+  }
+})
+
+// CLOSE MODAL
+closeIcon.addEventListener("click", () => {
+  postBox.style.display = "none";
+  // Activate the scroll button on modal close
+  document.body.style.overflowY = "scroll";
+})
+
+// Increase Text area size dynamically
+postArea.addEventListener("input", () => {
+  postArea.style.height = "auto";
+  postArea.style.height = (postArea.scrollHeight) + "px";
+  postArea.style.maxHeight = "256px";
+})
